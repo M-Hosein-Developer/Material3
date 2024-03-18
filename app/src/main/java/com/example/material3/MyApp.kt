1e2444817c76fa3eb4ba9506a5e07297677a1061
Mohammad Hosein <m.hosein.developer@gmail.com>
2024-03-18 00:12:19 -1200

    custom theme material 3

--- app/src/main/java/com/example/material3/MyApp.kt
+++ app/src/main/java/com/example/material3/MyApp.kt
@@ -10,7 +10,7 @@ class MyApp : Application() {
         super.onCreate()
 
 
-        DynamicColors.applyToActivitiesIfAvailable(this)
+//        DynamicColors.applyToActivitiesIfAvailable(this)
 
     }
 
